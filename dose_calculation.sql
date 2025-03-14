2914df04d92b71fec7b7e2eeebfa7d2000e19ea3
Nadiya Zahn <nadiya.zahn@gmail.com>
2025-03-14 18:42:20 +0100

    improvements

--- dose_calculation.sql
+++ dose_calculation.sql
@@ -1,20 +1,25 @@
-select sh1.chip_1 as measur_dose_1,
-	sh1.chip_2 as measur_dose_2,
-	sh1.chip_3 as measur_dose_3, 
-	sh1.chip_4 as measur_dose_4,
+select ls.chip_1 as measur_dose_1,
+	ls.chip_2 as measur_dose_2,
+	ls.chip_3 as measur_dose_3, 
+	ls.chip_4 as measur_dose_4,
 	tldc.chip_1 as calibr_dose_1, 
 	tldc.chip_2 as calibr_dose_2, 
 	tldc.chip_3 as calibr_dose_3, 
 	tldc.chip_4 as calibr_dose_4
-from tld.laser_shot_1 as sh1
-inner join tld.tld_calibration as tldc
-on sh1.card_number = tldc.card_number;
+from tld_measures.laser_shots as ls
+inner join tld_measures.tld_calibration as tldc
+on ls.card_number = tldc.card_number
+where ls.shot_number = 1;
 
-select ((5e-3)/tldc.chip_1)*sh1.chip_1 as measur_dose_1,
-	((5e-3)/tldc.chip_2)*sh1.chip_2 as measur_dose_2,
-	((5e-3)/tldc.chip_3)*sh1.chip_3 as measur_dose_3, 
-	((5e-3)/tldc.chip_4)*sh1.chip_4 as measur_dose_4
-from tld.laser_shot_1 as sh1
-inner join tld.tld_calibration as tldc
-on sh1.card_number = tldc.card_number;
+---
+
+select ((5e-3)/tldc.chip_1)*ls.chip_1 as measur_dose_1,
+	((5e-3)/tldc.chip_2)*ls.chip_2 as measur_dose_2,
+	((5e-3)/tldc.chip_3)*ls.chip_3 as measur_dose_3, 
+	((5e-3)/tldc.chip_4)*ls.chip_4 as measur_dose_4
+from tld_measures.laser_shots as ls
+inner join tld_measures.tld_calibration as tldc
+on ls.card_number = tldc.card_number
+where ls.shot_number = 1;
+
 
